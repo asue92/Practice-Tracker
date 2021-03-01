0a11540ea7483a489af10ed498a184ce3a1e4b6c
Adam Sue <adam.p.sue@gmail.com>
2021-03-01 15:55:31 -0500

    first commit

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+const functions = require("firebase-functions");
+const app = require("express")();
+
+const { getAllTodos } = require("./APIs/todos");
+
+app.get("/todos", getAllTodos);
+exports.api = functions.https.onRequest(app);
