47df922f76e5f69cf64b6ef3604025a05c900ef2
Adam Sue <adam.p.sue@gmail.com>
2021-03-01 18:31:24 -0500

    post request working

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,27 +1,5 @@
 const { db } = require("../util/admin");
 
-// exports.getAllTodos = (request, response) => {
-//   db.collection("todos")
-//     .orderBy("createdAt", "desc")
-//     .get()
-//     .then((data) => {
-//       let todos = [];
-//       data.forEach((doc) => {
-//         todos.push({
-//           todoId: doc.id,
-//           title: doc.data().title,
-//           body: doc.data().body,
-//           createdAt: doc.data().createdAt,
-//         });
-//       });
-//       return response.json(todos);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       return response.status(500).json({ error: err.code });
-//     });
-// };
-
 exports.getAllTodos = async (request, response) => {
   try {
     const data = await db
@@ -41,5 +19,31 @@ exports.getAllTodos = async (request, response) => {
   } catch (error) {
     console.error(err);
     return response.status(500).json({ error: err.code });
+  }
+};
+
+exports.postOneTodo = async (request, response) => {
+  try {
+    if (request.body.body.trim() === "") {
+      return response.status(400).json({ body: "Must not be empty" });
+    }
+
+    if (request.body.title.trim() === "") {
+      return response.status(400).json({ title: "Must not be empty" });
+    }
+    const newTodoItem = {
+      title: request.body.title,
+      body: request.body.body,
+      createdAt: new Date().toISOString(),
+    };
+
+    const createdItem = await db.collection("todos").add(newTodoItem);
+
+    const item = await db.collection("todos").doc(createdItem.id).get();
+    const { title, body } = item.data();
+    return response.json({ title, body });
+  } catch (error) {
+    response.status(500).json({ error: "Something went wrong" });
+    console.error(error);
   }
 };
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
-const { getAllTodos } = require("./APIs/todos");
+const { getAllTodos, postOneTodo } = require("./APIs/todos");
 
 app.get("/todos", getAllTodos);
+app.post("/todos", postOneTodo);
+
 exports.api = functions.https.onRequest(app);
