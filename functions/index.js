aff0beeedb2b4d63dd3ea98c9f03fe7f31fcfb2f
Adam Sue <adam.p.sue@gmail.com>
2021-03-01 19:01:30 -0500

    added api routes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
-const { getAllTodos, postOneTodo } = require("./APIs/todos");
+const {
+  getAllTodos,
+  postOneTodo,
+  deleteTodo,
+  editTodo,
+} = require("./APIs/todos");
 
 app.get("/todos", getAllTodos);
-app.post("/todos", postOneTodo);
+app.post("/todo", postOneTodo);
+app.delete("/todo/:todoId", deleteTodo);
+app.put("/todo/:todoId", editTodo);
 
 exports.api = functions.https.onRequest(app);
