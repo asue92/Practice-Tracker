2281c9f92028cff6775a98cf2ad676798d0e890c
Adam Sue <adam.p.sue@gmail.com>
2021-03-01 19:01:30 -0500

    added api routes

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -17,8 +17,8 @@ exports.getAllTodos = async (request, response) => {
     });
     return response.json(todos);
   } catch (error) {
-    console.error(err);
-    return response.status(500).json({ error: err.code });
+    console.error(error);
+    return response.status(500).json({ error: error.code });
   }
 };
 
@@ -45,5 +45,35 @@ exports.postOneTodo = async (request, response) => {
   } catch (error) {
     response.status(500).json({ error: "Something went wrong" });
     console.error(error);
+  }
+};
+
+exports.deleteTodo = async (request, response) => {
+  try {
+    const document = db.doc(`/todos/${request.params.todoId}`);
+    const foundDoc = await document.get();
+    if (!foundDoc.exists) {
+      return response.status(404).json({ error: "Todo not found" });
+    }
+    document.delete();
+    response.json({ message: "delete succesful!" });
+  } catch (error) {
+    return response.status(500).json({ error: error.code });
+  }
+};
+
+exports.editTodo = async (request, response) => {
+  try {
+    if (request.body.todoId || request.body.createdAt) {
+      response.status(403).json({ message: "Not allowed to edit" });
+    }
+    let document = await db.collection("todos").doc(`${request.params.todoId}`);
+    await document.update(request.body);
+    response.json({ message: "Updated successfully" });
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({
+      error: err.code,
+    });
   }
 };
