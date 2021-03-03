c5af00132ebae6f07e2da1aa46a1b3f71bb5a9ef
Adam Sue <adam.p.sue@gmail.com>
2021-03-03 15:46:10 -0500

    combined front end/back end, log in route returns token

--- view/src/App.js
+++ view/src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import login from "./pages/login";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={login} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 export default App;
---
+++ view/src/pages/styling.js
@@ -0,0 +1,27 @@
+export const loginStyles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10,
+  },
+  progess: {
+    position: "absolute",
+  },
+});
