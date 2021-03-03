db4c3caf713c32d8fb886fa86ddc1fa1d4bbab6a
Adam Sue <adam.p.sue@gmail.com>
2021-03-03 16:44:19 -0500

    signup page done and tested

--- view/src/App.js
+++ view/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import login from "./pages/login";
+import signup from "./pages/signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
         </Switch>
       </div>
     </Router>
